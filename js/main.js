a20152048b5a316a9ca39abb9a95c65c49fddcf3
albyschianni <alberto.schianni@gmail.com>
2022-11-21 16:59:04 +0100

    update

--- js/main.js
+++ js/main.js
@@ -2,6 +2,61 @@
 // Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
 // MILESTONE 0:
 // Creare l’array di oggetti con le informazioni fornite.
+
+const teamMeamber = [
+    {
+        "nameMember" : "Wayne Burnett",
+        "role" : "Fouder & CEO",
+        "image" : "image-wayne"
+    },
+    {
+        "nameMember" : "Angela Caroll",
+        "role" : "Chief Editor",
+        "image" : "image-angela"
+    },
+    {
+        "nameMember" : "Walter Gordon",
+        "role" : "Office Manager",
+        "image" : "image-walter"
+    },
+    {
+        "nameMember" : "Angela Lopez",
+        "role" : "Social Media Manager",
+        "image" : "image-angela L"
+    },
+    {
+        "nameMember" : "Scott Estrada",
+        "role" : "Developer",
+        "image" : "image-scott"
+    },
+    {
+        "nameMember" : "Barbara Ramos",
+        "role" : "Graphic Designer",
+        "image" : "image-barbara"
+    }
+]
+console.log(teamMeamber);
+
+function createElementFromHTML(htmlString) {
+    div.innerHTML = htmlString.trim();
+}
+
+const addToDom = function ({
+    nameMember,
+    role,
+    image
+}) {
+
+function render (){
+    return createElementFromHTML(`
+    
+        <div class="nome${nameMember}">
+        <div class="ruolo${role}">
+    `)
+}
+}
+document.querySelector("#container-schede").appendChild(render())
+
 // MILESTONE 1:
 // Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
 // MILESTONE 2:
